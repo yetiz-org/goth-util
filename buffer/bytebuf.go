1c63fb1800c7bb329144cde1889a12316d92a701
Yeti Sno <yeti@yetiz.org>
2021-04-09 00:54:06 +0800

    update

--- buffer/bytebuf.go
+++ buffer/bytebuf.go
@@ -1,6 +1,41 @@
 package buffer
 
-type ByteBuf struct {
-	buf                   []byte
-	readIndex, writeIndex int
+import "io"
+
+type ByteBuf interface {
+	io.ByteWriter
+	io.Writer
+	io.ByteReader
+	io.Reader
+	io.StringWriter
+	MarkReaderIndex() ByteBuf
+	ResetReaderIndex() ByteBuf
+	MarkWriterIndex() ByteBuf
+	ResetWriterIndex() ByteBuf
+	Reset() ByteBuf
+	Bytes() []byte
+	Len() int
+	Cap() int
+	Grow(i int)
+	WriteBytes(bs []byte) error
+	WriteByteBuf(buf ByteBuf) error
+	WriteInt(i int) error
+	WriteUInt(i uint) error
+	WriteInt64(i int64) error
+	WriteUInt64(i uint64) error
+	ReadBytes(len int) ([]byte, error)
+	ReadByteBuf(len int) (ByteBuf, error)
+	ReadInt() (int, error)
+	ReadUInt() (uint, error)
+	ReadInt64() (int64, error)
+	ReadUInt64() (uint64, error)
 }
+
+type DefaultByteBuf struct {
+	buf                                                            []byte
+	readerIndex, writerIndex, markedReaderIndex, markedWriterIndex int
+}
+//
+//func NewByteBuf() ByteBuf {
+//	return &DefaultByteBuf{}
+//}
