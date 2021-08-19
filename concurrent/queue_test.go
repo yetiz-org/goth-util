70f37678376dd16aa14166f5662a912224fac85c
Yeti Sno <yeti@yetiz.org>
2021-08-20 00:31:37 +0800

    add blockingqueue

--- concurrent/queue_test.go
+++ concurrent/queue_test.go
@@ -2,6 +2,7 @@ package concurrent
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -20,4 +21,26 @@ func TestQueue(t *testing.T) {
 	q.Reset()
 	assert.EqualValues(t, 0, q.Len())
 	assert.Nil(t, q.Pop())
+}
+
+func TestBlockingQueue(t *testing.T) {
+	q := &BlockingQueue{}
+	q.Push(1)
+	q.Push(2)
+	assert.EqualValues(t, 2, q.Len())
+	assert.EqualValues(t, q.Pop(), 1)
+	assert.EqualValues(t, q.Pop(), 2)
+	assert.EqualValues(t, 0, q.Len())
+	q.Push(1)
+	q.Push(2)
+	assert.EqualValues(t, q.Pop(), 1)
+	q.Reset()
+	assert.EqualValues(t, 0, q.Len())
+	go func() {
+		time.Sleep(time.Millisecond)
+		q.Push(1)
+	}()
+
+	assert.NotNil(t, q.Pop())
+	assert.EqualValues(t, 0, q.Len())
 }
