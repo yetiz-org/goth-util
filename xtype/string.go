f1fe2fa9c22c4f602eb7890643387b28604d44ac
Yeti Sno <yeti@yetiz.org>
2021-11-16 23:11:50 +0800

    update string

--- xtype/string.go
+++ xtype/string.go
@@ -17,8 +17,12 @@ func (s String) Lower() string {
 }
 
 func (s String) Short() string {
+	return s.ShortBySign("-")
+}
+
+func (s String) ShortBySign(sign string) string {
 	sb := ""
-	for _, sp := range strings.Split(strings.ToUpper(string(s)), "-") {
+	for _, sp := range strings.Split(strings.ToUpper(string(s)), sign) {
 		if len(sp) > 0 {
 			sb = sb + sp[0:1]
 		}
